c24469179204a4be085ba159bc05c2aef8c445dc
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-27 17:20:48 +0530

    Add files via upload

---
+++ multipile1.java
@@ -0,0 +1,18 @@
+package janani;
+import java.util.Scanner;
+public class multipile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,n1,n2,a;
+System.out.println("enter the numbers");
+Scanner ja=new Scanner(System.in);
+//System.out.println("enter the no");
+n=ja.nextInt();
+n1=ja.nextInt();
+n2=ja.nextInt();
+a=(n*n1*n2)/100;
+System.out.println(a);
+	}
+
+}
